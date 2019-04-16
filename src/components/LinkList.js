11e825cef762a3594c3b8cfed37fa3a25b40f303
Igor Morgunov <imorgunov@dc2.propsol.org>
2019-04-16 16:59:05 -0600

    'next'

---
+++ src/components/LinkList.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import Link from './Link'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const FEED_QUERY = gql`
+  {
+    feed {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+class LinkList extends Component {
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+    {({ loading, error, data }) => {
+      if (loading) return <div>Fetching</div>
+      if (error) return <div>Error</div>
+
+      const linksToRender = data.feed
+
+      return (
+        <div>
+          {linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} />)}
+        </div>
+      )
+    }}
+  </Query>
+  )
+  }
+  
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+  const data = store.readQuery({ query: FEED_QUERY })
+
+  const votedLink = data.feed.links.find(link => link.id === linkId)
+  votedLink.votes = createVote.link.votes
+
+  store.writeQuery({ query: FEED_QUERY, data })
+}
+
+}
+
+export default LinkList
--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { AUTH_TOKEN } from './constants'
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
